de91a5651e786abf836e8c47072e635da67932ab
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2022-02-01 21:08:09 +0500

    Initial Commit

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit , Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+
+export class ButtonComponent implements OnInit {
+  // @ts-ignore
+  @Input() text: string;
+  // @ts-ignore
+  @Input() color: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
